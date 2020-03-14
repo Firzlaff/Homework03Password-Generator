d85a07571f3f3e1458de58e4c553d4bc319e9f1b
Firzlaff <tysenfirzlaff@gmail.com>
2020-03-14 09:54:21 -0600

    updating confirms

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,39 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate"); {
+ //   document.getElementById("#generate").addEventListerner("click", "charLen")
+
+        var charLen = prompt("Enter Password length between 8-128"); {
+     //     charLen = length of the password that needs to be created
 
 
-var characterLength = prompt("please choose password length between 8-128");
+        }
 
-var lowerCase = confrim("Letters contain lowercase");
+     
+}
 
-var upperCase = confrim("Letters contain uppercase");
-
-var containsNumbers = confrim("Characters contain numbers");
-
-var containsCharacters = confrim("Characters contain special characters");
-
+  var lowLet = confirm("Letters contain lowercase");
+// lowLet = should the password contain lowercase letters
+console.log(charLen)
 
 
+  var upLet = confirm("Letters contain uppercase");
+// upLet = should the password contain uppercase letters
 
-var charlow = ("abcdefghijklmnopqrstuvwxyz");
-var charup = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-var num = ("0123456789");
-var sym = ("!@#$%?&*");
+
+  var contNums = confirm("Characters contain numbers");
+  // contNums = should the password contain numbers
+
+  var contSpec = confirm("Characters contain special characters");
+  // contSpec = should the pass word contain special characters 
+
+
+
+// this was just for another idea i was thinking about and if i will need to incorp 
+// into a for loop with randomization flor.math random.math
+// var charlow = ("abcdefghijklmnopqrstuvwxyz");
+// var charup = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+// var num = ("0123456789");
+//var sym = ("!@#$%?&*");
 
 
 // Write password to the #password input
@@ -31,6 +46,10 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); {
+
+
+}
+
 
 
