813a8e3093ce7afacbd1206cb07f95a920f23711
Firzlaff <tysenfirzlaff@gmail.com>
2020-03-15 14:25:29 -0600

    cleaning up code and removing old ideas to better see my flow

--- Develop/script.js
+++ Develop/script.js
@@ -4,55 +4,43 @@ var charup = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', '
 var num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
 var sym = ['!', '@', '#', '$', '%', '?', '&', '*'];
 
-//   document.getElementById("#generate").addEventListerner("click", "charLen")
+
 var generateBtn = document.querySelector("#generate"); {
  }
-//     charLen = length of the password that needs to be created
+// prompts for char length
 var charLen = prompt("Enter Password length between 8-128"); 
-
+// confirms for char
 var lowLet = confirm("Letters contain lowercase");
-// lowLet = should the password contain lowercase letters
-//console.log(charLen)
-
 var upLet = confirm("Letters contain uppercase");
-//upLet = should the password contain uppercase letters
-//console.log(upLet)
-
 var contNums = confirm("Characters contain numbers");
-// contNums = should the password contain numbers
-//  console.log(contNums)
-
 var contSpec = confirm("Characters contain special characters");
-//  contSpec = should the pass word contain special characters 
-//  console.log(contSpec)
 
 // Write password to the #password input
-//function writePassword() {
-  //var password = generatePassword();
+function writePassword() {
+  var password = generatePassword();
 
-//  var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector("#password");
 
-//  passwordText.value = password;
+  passwordText.value = password;
 
-//}
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword); {
 
-//  to string
-// function generateRdm(){
-//  Math.random().toString(36).slice(2)
-
-} 
-for (var i = 0; i < 1; i++) {
-  
-  var generateRdm = charlow[Math.floor(Math.random() * charlow.length)];
-    console.log(generateRdm)
 }
 
-// function to write the password
+//Generating a random letter using my array. 
+// function generateRdm(){
+//  Math.random().toString(36).slice(2)
+ 
+for (var i = 0; i < 1; i++) {
+  
+  var generateRdm = char[Math.floor(Math.random() * char.length)];
+  //  console.log(generateRdm)
+}
 
-funtion writePassword() {
+// if and else ifs for password generation
 
 //all four char options 
 if (lowLet && upLet && contNums && contSpec){
@@ -62,7 +50,7 @@ if (lowLet && upLet && contNums && contSpec){
 else if(lowLet && contNums && contSpec){
   chars = concat(charlow, contNums, contSpec);
 }
-else if(lowLet && upLet && contSpec){
+else if(lowLet && upLet && contSpec){ 
   chars = concat(charlow, charup, sym);
 }
 else if (lowLet && contNums && contSpec){
@@ -76,6 +64,5 @@ else if(lowLet && contNums){
   char = concat(charlow, num);
 }
 else if(lowLet && contSpec){
-  char = concat(charlow, sym)
-}
+  char = concat(charlow, sym);
 }
