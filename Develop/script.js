07bf505db274be932220901087af0c6311b50213
Firzlaff <tysenfirzlaff@gmail.com>
2020-03-15 23:10:29 -0600

    updating more variables and starting to write password function

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,10 @@ var charlow = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
 var charup = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ];
 var num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
 var sym = ['!', '@', '#', '$', '%', '?', '&', '*'];
+// i added them hiere sice when i added the var dow in the confirms with the function loop it nested them
+var upLet = charup
+var contNums = num
+var contSpec = sym 
 
 
 var generateBtn = document.querySelector("#generate"); {
@@ -22,8 +26,8 @@ function charPrompt(){
      var charLen = prompt("Enter Password length between 8-128");}
 }
 // confirms for char
-var lowLet = confirm("Letters contain lowercase");
-var upLet = confirm("Letters contain uppercase");
+
+var upLet = confirm("Letters contain uppercase, lowercase are automatically included");
 var contNums = confirm("Characters contain numbers");
 var contSpec = confirm("Characters contain special characters");
 }
@@ -44,14 +48,14 @@ for (var i = 0; i < 1; i++) {
   var generateRdm = char[Math.floor(Math.random() * char.length)];
   //  console.log(generateRdm)
 }
-// if and else ifs for password generation
-//all four char options 
+// if and else ifs for password generation my idea here is to just take the input and then add the char to the loop
+//all 3 char options and lowercase
 if (lowLet && upLet && contNums && contSpec){
   chars = (charup, charlow, num, sym);
 }
-// 3 char options
+// 2 char options and included lowercase
 else if(lowLet && contNums && contSpec){
-  chars = concat(charlow, contNums, contSpec);
+  chars = concat(charlow, num, sym);
 }
 else if(lowLet && upLet && contSpec){ 
   chars = concat(charlow, charup, sym);
@@ -59,13 +63,13 @@ else if(lowLet && upLet && contSpec){
 else if (lowLet && contNums && contSpec){
   chars = concat(charlow, num, sym);
 }
-//2 char options
+//1 char option and included lowercase
 else if(lowLet && upLet){
   chars = concat(charlow, charup);
 }
 else if(lowLet && contNums){
-  char = concat(charlow, num);
+  chars = concat(charlow, num);
 }
 else if(lowLet && contSpec){
-  char = concat(charlow, sym);
+  chars = concat(charlow, sym);
 }
