0a1ac692d270b8672d8242fcec5a37925bc6c4d7
Firzlaff <tysenfirzlaff@gmail.com>
2020-03-16 11:22:54 -0600

    attemting to add an object for the characterstobechosen for password generation

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,17 @@
-// Char arrays
+// I feel I need to create an object that is chars that has the key and values for the char types and what they are. 
+//var chars= {
+  // Char arrays
 var charlow = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var charup = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ];
 var num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
 var sym = ['!', '@', '#', '$', '%', '?', '&', '*'];
-// i added them here sice when i added the var dow in the confirms with the function loop it nested them there. 
-var upLet = charup
-var contNums = num
-var contSpec = sym 
-//var lowlet = charlow
-var charLen
+//} 
+// i added them here sice when i added the var down in the confirms with the function loop it nested them there. 
+//var upLet = charup
+//var contNums = num
+//var contSpec = sym 
+var lowlet = true
+//var charLen
 
 var generateBtn = document.querySelector("#generate"); {
 
@@ -27,24 +30,25 @@ function charPrompt(){
      var charLen = prompt("Enter Password length between 8-128");}
 }
 // confirms for char
-
-var upLet = confirm("Letters contain uppercase, lowercase are automatically included");
+//var lowLet = confirm("Letters contain lowercase");
+var upLet = confirm("Letters contain uppercase");
 var contNums = confirm("Characters contain numbers");
 var contSpec = confirm("Characters contain special characters");
 }
 
+
 // Write password to the #password input
 function writePassword() {
-  var password = password;
-
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
 }
 
 //Generating a random letter using my array.
 //for (var i = 0; i < 10; i++) {                                                                                     //idea for later maybe
-  var generateRdm = charlow[Math.floor(Math.random() * charlow.length)];                                              // function generateRdm(){
+  var generateRdm = chars[Math.floor(Math.random() * chars.length)];                                              // function generateRdm(){
     console.log(generateRdm)                                                                                   //  Math.random().toString(36).slice(2)
 //}
 // if and else ifs for password generation my idea here is to just take the input and then add the char to the loop
