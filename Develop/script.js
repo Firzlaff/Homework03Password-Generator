733730e27f8e9c2fa8751b1416e0aaabfb22b9f0
Firzlaff <tysenfirzlaff@gmail.com>
2020-03-16 16:00:49 -0600

    updatd if and else statements, implemented my single random letter function and applied it to the if else statements.

--- Develop/script.js
+++ Develop/script.js
@@ -1,80 +1,70 @@
 // I feel I need to create an object that is chars that has the key and values for the char types and what they are. 
 //var chars= {
-  // Char arrays
+// Char arrays
 var charlow = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var charup = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ];
+var charup = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
 var sym = ['!', '@', '#', '$', '%', '?', '&', '*'];
 //} 
 // i added them here sice when i added the var down in the confirms with the function loop it nested them there. 
-//var upLet = charup
-//var contNums = num
-//var contSpec = sym 
-var lowlet = true
-//var charLen
+var upLet;
+var contNums;
+var contSpec;
+var lowlet;
+var charLen;
 
-var generateBtn = document.querySelector("#generate"); {
+
+var generateBtn = document.querySelector("#generate");
+
+// Event listner for event click to start prompts//
+generateBtn.addEventListener("click", writePassword);
+
+// prompts for char length with required peramiters//
+function charPrompt() {
+   charLen = parseInt(prompt("Enter Password length between 8-128"));
+   while (charLen <= 7 || charLen >= 129) {
+   alert("Please attempt again, Password length must be between 8-128 characters");
+    charLen = prompt("Enter Password length between 8-128");
+  }
+
+  // confirms for char//
+  lowLet = confirm("Letters contain lowercase");
+  upLet = confirm("Letters contain uppercase");
+  contNums = confirm("Characters contain numbers");
+  contSpec = confirm("Characters contain special characters");
 
 }
 
-// Event listner for event click to start prompts
-generateBtn.addEventListener("click", charPrompt); {
+//function to pull a random char from the chosen array//
+function rdmChar (arr) {
+  return arr[Math.floor(Math.random() * arr.length)]
 
 }
+
+//generate password function//
+function generatePassword() {
+  var password = []
+  charPrompt()
+  if (lowLet) password.push(rdmChar(charlow));
+  if (upLet) password.push(rdmChar(charup));
+  if (contNums) password.push(rdmChar(num));
+  if (contSpec) password.push(rdmChar(sym));
  
-// prompts for char length with required peramiters
-function charPrompt(){
-   var charLen = prompt("Enter Password length between 8-128"); { 
-   while(charLen <= 7 || charLen >= 129) {
-     alert("Please attempt again, Password length must be between 8-128 characters");
-     var charLen = prompt("Enter Password length between 8-128");}
+    while (password.length < charLen) {
+      if (lowLet && password.length < charLen)password.push(rdmChar(charlow));
+      if (upLet && password.length < charLen)password.push(rdmChar(charup));
+      if (contNums && password.length < charLen)password.push(rdmChar(num));
+      if (contSpec && password.length < charLen)password.push(rdmChar(sym));
+  }
+  return password.join("")
 }
-// confirms for char
-//var lowLet = confirm("Letters contain lowercase");
-var upLet = confirm("Letters contain uppercase");
-var contNums = confirm("Characters contain numbers");
-var contSpec = confirm("Characters contain special characters");
-}
-
 
 // Write password to the #password input
 function writePassword() {
+
   var password = generatePassword();
+console.log(password)
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
-
-//Generating a random letter using my array.
-//for (var i = 0; i < 10; i++) {                                                                                     //idea for later maybe
-  var generateRdm = chars[Math.floor(Math.random() * chars.length)];                                              // function generateRdm(){
-    console.log(generateRdm)                                                                                   //  Math.random().toString(36).slice(2)
-//}
-// if and else ifs for password generation my idea here is to just take the input and then add the char to the loop
-//all 3 char options and lowercase
-if (lowLet && upLet && contNums && contSpec){
-  var chars = (charup += charlow += num += sym);
-}
-// 2 char options and included lowercase
-else if(lowLet && contNums && contSpec){
-  var chars = (charlow += num += sym);
-}
-else if(lowLet && upLet && contSpec){ 
-  var chars = (charlow += charup += sym);
-}
-else if (lowLet && contNums && contSpec){
-  var chars = (charlow += num += sym);
-}
-//1 char option and included lowercase
-else if(lowLet && upLet){
-  var chars = (charlow += charup);
-}
-else if(lowLet && contNums){
-  var chars = (charlow += num);
-}
-else if(lowLet && contSpec){
-  var chars = (charlow += sym);
-}
-
-// console.log(generateRdm)
